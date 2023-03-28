82eb662e9cf564c932d2fd93eecc88428cdc7b8b
Ian Greenfield <ian.greenfield@heydayskincare.com>
2023-03-28 10:55:33 -0500

    fetch

--- graphql/apollo-client.js
+++ graphql/apollo-client.js
@@ -1,8 +1,10 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import fetch from "cross-fetch";
 
 const client = new ApolloClient({
     link: createHttpLink({
       uri: 'https://prod-00.heyday.solvestack.net/graphql',
+      fetch,
       headers: {
         'Content-Type': 'application/json',
         'Authorization' : 'Basic c29sdmUta2V5LWE3MjUxMGMwLTZjODctNDljNS04ZjFmLTE1ZWE3N2FiMjQ2OTpzb2x2ZS1zZWNyZXQtMWMxMzk2ZDctODk4Mi00ZTYwLWJlZjQtZjEzNmIwZjI0ZmI1'
